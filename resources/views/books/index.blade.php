df0f563914879acca6c344f2050380a3b44038e2
Koyo Kisu <koyokisu@gmail.com>
2020-04-09 23:58:33 +0900

    navバー追加

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,15 +3,20 @@
 @section('title', 'メイン画面')
 
 @section('content')
-<div class="container">
-  @foreach($books as $book)
-    <p>タイトル</p>
-    <div>{{ $book->title }}</div>
-    <p>著者名</p>
-    <div>{{ $book->author }}</div>
-    <p>出版社名</p>
-    <p>{{ $book->description }}</p>
-    <p></p>
-  @endforeach
-</div>
+  @include('nav')
+  <div class="container">
+    @foreach($books as $book)
+      <div class="card mt-3">
+        <div class="card-body flex-row">
+          <p>タイトル</p>
+          <div>{{ $book->title }}</div>
+          <p>著者名</p>
+          <div>{{ $book->author }}</div>
+          <p>出版社名</p>
+          <p>{{ $book->description }}</p>
+          <p></p>
+        </div>
+      </div>
+    @endforeach
+  </div>
 @endsection
