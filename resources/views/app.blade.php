635d5ab2dc1a4529cf8637e53fed4fba94f14c8d
Koyo Kisu <koyokisu@gmail.com>
2020-04-12 16:56:53 +0900

    トップページ処理追加

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,6 +18,8 @@
 <body>
 
   @yield('content')
+  <!-- JQuery -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <!-- Bootstrap tooltips -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
   <!-- Bootstrap core JavaScript -->
