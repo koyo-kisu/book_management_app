e53274464029da76c3d80e838f6387863e58856d
Koyo Kisu <koyokisu@gmail.com>
2020-05-03 23:20:14 +0900

    いいね機能追加

--- app/Book.php
+++ app/Book.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Book extends Model
 {
@@ -21,4 +22,22 @@ class Book extends Model
         return $this->belongsTo('App\User');
     }
 
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
+    }
+
+    // ユーザーがこの記事をいいね済みかどうかを返すメソッド
+    public function isLikedBy(?User $user): bool
+    {
+        return $user
+            ? (bool)$this->likes->where('id', $user->id)->count()
+            : false;
+    }
+
+    // 記事にいいねをしたユーザーの総数、つまりいいねの合計が求まります
+    public function getCountLikesAttribute(): int
+    {
+        return $this->likes->count();
+    }
 }
