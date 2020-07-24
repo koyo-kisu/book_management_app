0b337114d7dc76cdd022fc958479f16017b5aa75
kohki_ohno <51240083+nocch12@users.noreply.github.com>
2020-07-24 17:22:39 +0900

    Update RedirectIfAuthenticated.php
    仮コード削除

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,8 +18,6 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        Auth::guard('admin')->logout();
-
         if (Auth::guard($guard)->check()) {
             return redirect(RouteServiceProvider::HOME);
         }
