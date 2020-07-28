5ac5054f654459eb82579f2c87777f3b1e32fe51
Koyo Kisu <koyokisu@gmail.com>
2020-07-29 07:29:28 +0900

    navバーコンテンツを権限別に表示変更

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,37 +1,24 @@
 <nav class="navbar navbar-expand">
-  <!-- 未ログインユーザー用 -->
-  @guest
+  @if(auth('admin'))
+    <a href="{{ route('books.index') }}">BOOKSHELF-APP</a>
+  @elseif(auth('user'))
     <a href="/">BOOKSHELF-APP</a>
-  @endguest
-
-  <!-- ログイン済みユーザー用 -->
-  @auth('user')
+  @else
     <a href="/">BOOKSHELF-APP</a>
-  @endauth
-
-  <!-- ログイン済み管理者用 -->
-  @auth('admin')
-    <a href="{{ route('admin.index') }}">BOOKSHELF-APP</a>
-  @endauth
+  @endif
 
   <ul class="navbar-nav ml-auto">
-    <!-- 未ログインユーザー用 -->
-    @guest
-    <li class="nav-item">
-      <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="{{ route('login') }}">ログイン</a>
-    </li>
-    @endguest
-
-    @auth('admin')
+  @if(auth('admin'))
     <li class="nav-item">
       <a class="nav-link" href="{{ route('books.create') }}"><i class="fas fa-pen mr-1"></i>投稿する</a>
     </li>
-    @endauth
-    <!-- ログイン済みユーザー用 -->
-    @auth('user')
+    <li class="content_list mb-2 mt-2">
+      <form id="logout-button" method="POST" action="{{ route('admin.logout') }}">
+        <button form="logout-button" type="submit">ログアウト</button>
+        @csrf
+      </form>
+    </li>
+  @elseif(auth('user'))
     <!-- ここからDropdown -->
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
@@ -53,17 +40,13 @@
       @csrf
     </form>
     <!-- ここまでDropdown -->
-    @endauth
-
-   <!-- ログイン済み管理者用 -->
-    @auth('admin')
-    <li class="content_list mb-2 mt-2">
-      <form id="logout-button" method="POST" action="{{ route('admin.logout') }}">
-        <button form="logout-button" type="submit">ログアウト</button>
-        @csrf
-      </form>
+  @else
+    <li class="nav-item">
+      <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
     </li>
-    @endauth
-
+    <li class="nav-item">
+      <a class="nav-link" href="{{ route('login') }}">ログイン</a>
+    </li>
+  @endif
   </ul>
 </nav>
