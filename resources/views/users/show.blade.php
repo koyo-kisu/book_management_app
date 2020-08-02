3a83e45ba9192b965bffdd34f3668b7b2602a79d
nocch12 <k.ohno12.gt@gmail.com>
2020-08-02 11:42:37 +0900

    タブ切り替えUI作成

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -19,18 +19,78 @@
         </h2>
       </div>
     </div>
-    <ul class="nav nav-tabs nav-justified mt-3">
+    <ul class="nav nav-tabs nav-justified mt-3" id="userTabs" role="tablist">
       <li class="nav-item">
-        <div class="nav-link text-muted active"
-           href="#">
-          いいねした本
+        <a
+          class="nav-link text-muted active"
+          id="likes-tab"
+          data-toggle="tab"
+          role="tab"
+          aria-controls="likes"
+          aria-selected="true"
+          href="#nav-likes"
+        >
+          いいね
+        </a>
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link text-muted"
+          id="history-tab"
+          data-toggle="tab"
+          role="tab"
+          aria-controls="history"
+          aria-selected="false"
+          href="#nav-history"
+        >
+          貸出履歴
+        </a>
+      </li>
+      <li class="nav-item">
+        <div
+          class="nav-link text-muted"
+          id="bookings-tab"
+          data-toggle="tab"
+          role="tab"
+          aria-controls="bookings"
+          aria-selected="false"
+          href="#nav-bookings"
+        >
+          予約一覧
         </div>
       </li>
     </ul>
-    <div class="row mb-5 mt-3">
-      @foreach($books as $book)
-        @include('books.card')
-      @endforeach
+    <div class="tab-content" id="userTabsContent">
+      <div
+        class="tab-pane fade show active row mb-5 mt-3"
+        id="nav-likes"
+        role="tabpanel"
+        aria-labelledby="nav-likes-tab"
+      >
+        @foreach($books as $book)
+          @include('books.card')
+        @endforeach
+      </div>
+      <div
+        class="tab-pane fade row mb-5 mt-3"
+        id="nav-history"
+        role="tabpanel"
+        aria-labelledby="nav-history-tab"
+      >
+        @foreach($books as $book)
+          @include('books.card')
+        @endforeach
+      </div>
+      <div
+        class="tab-pane fade row mb-5 mt-3"
+        id="nav-bookings"
+        role="tabpanel"
+        aria-labelledby="nav-bookings-tab"
+      >
+        @foreach($books as $book)
+          @include('books.card')
+        @endforeach
+      </div>
     </div>
   </div>
 @endsection
