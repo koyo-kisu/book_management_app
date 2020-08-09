2ef5b146c6cacf5b158bd15a8d84cfc95c1011ae
nocch12 <k.ohno12.gt@gmail.com>
2020-08-09 23:29:46 +0900

    貸出履歴と予約一覧をリスト形式に

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -88,11 +88,55 @@
         role="tabpanel"
         aria-labelledby="nav-bookings-tab"
       >
-        <div class="row mb-5 mt-3">
-          @foreach($books_booking as $book)
-            @include('books.card')
-          @endforeach
-        </div>
+
+        <table class="table table-borderless">
+          <thead>
+            <tr>
+              <th scope="col">書籍名</th>
+              <th scope="col">貸出日</th>
+              <th scope="col">返却日</th>
+              <th scope="col">状態</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- 一覧を表示 -->
+            @foreach($books_booking as $book)
+            <tr class="border-bottom border-primary">
+              <td class="align-middle py-0">{{ str_limit($book->title, 60, '...') }}</td>
+              <td class="align-middle py-0">{{ $book->pivot->booking_date_from }}</td>
+              <td class="align-middle py-0">{{ $book->pivot->booking_date_to }}</td>
+              @if($book->pivot->is_approved === 0)
+                <td class="align-middle py-0">未承認</td>
+              @else
+                <td class="align-middle py-0">承認済</td>
+              @endif
+              <td class="align-middle py-0 text-right">
+                <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#modal-cancel-{{ $book->pivot->id }}">キャンセル</button>
+
+                <!-- 削除モーダル -->
+                  @component('components.dialog', ['id' => 'modal-cancel-'.$book->pivot->id])
+                  @slot('body')
+                    <form method="POST" action="{{ route('bookings.cancel', ['booking' => $book->pivot->id]) }}">
+                      @csrf
+                      <!-- methodフィールド：postメソッドではなくdeleteメソッドとして解釈 -->
+                      @method('DELETE')
+                      <div class="modal-body text-left">
+                        予約をキャンセルします。よろしいですか？
+                      </div>
+                      <div class="modal-footer justify-content-between">
+                        <a class="btn btn-outline-grey" data-dismiss="modal">やめる</a>
+                        <button type="submit" class="btn btn-danger">キャンセルする</button>
+                      </div>
+                    </form>
+                  @endslot
+                @endcomponent
+              </td>
+            </tr>
+            @endforeach
+          </tbody>
+
+        </table>
       </div>
       <!-- end.予約一覧 -->
 
@@ -103,11 +147,25 @@
         role="tabpanel"
         aria-labelledby="nav-history-tab"
       >
-        <div class="row mb-5 mt-3">
-        @foreach($books_history as $book)
-          @include('books.card')
-        @endforeach
-        </div>
+        <table class="table table-borderless">
+          <thead>
+            <tr>
+              <th scope="col">書籍名</th>
+              <th scope="col">貸出日</th>
+              <th scope="col">返却日</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- 一覧を表示 -->
+            @foreach($books_history as $book)
+            <tr class="border-bottom border-primary">
+              <td class="align-middle py-0">{{ str_limit($book->title, 60, '...') }}</td>
+              <td class="align-middle py-0">{{ $book->pivot->booking_date_from }}</td>
+              <td class="align-middle py-0">{{ $book->pivot->booking_date_to }}</td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
       </div>
       <!-- end.貸出履歴 -->
     </div>
