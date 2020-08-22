cd7d2879f9d5be606720fd6a396c73e4009246f9
Koyo Kisu <koyokisu@gmail.com>
2020-08-22 16:16:36 +0900

    メール送信機能修正

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -74,9 +74,6 @@ class BookingController extends Controller
         ])->save();
 
         // メール送信処理
-        $aa = $request->merge(['user_id' => $booking->user->id]);
-        // dd($aa);
-
         try {
             Mail::send(new ReplyMail($booking));
         } catch (\Exception $e) {
@@ -97,9 +94,6 @@ class BookingController extends Controller
         ])->save();
 
         // メール送信処理
-        $user = \Auth::user();
-        $request->merge(['user_id' => $user->id]);
-
         try {
             Mail::send(new RejectMail($booking));
         } catch (\Exception $e) {
--- resources/views/mail/reply.blade.php
+++ resources/views/mail/reply.blade.php
@@ -5,8 +5,8 @@
 
 <div>書籍名: {{ $booking->book->title }}</div>
 <div>申請者: {{ $booking->user->name }}</div>
-<div>貸出希望日: {{ $booking->booking_date_from }}</div>
-<div>返却予定日: {{ $booking->booking_date_to }}</div>
+<div>貸出希望日: {{ $booking->booking_date_from->format('Y年m月d日') }}</div>
+<div>返却予定日: {{ $booking->booking_date_to->format('Y年m月d日') }}</div>
 <div>備考：{{ $booking->reply_comment }}</div>
 <br>
 
